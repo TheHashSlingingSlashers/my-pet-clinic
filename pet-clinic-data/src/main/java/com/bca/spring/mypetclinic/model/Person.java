f48b1459d87969913f70ea0ec3d1a1f5056dde62
Kasih Handoyo <kasih_handoyo@bca.co.id>
2022-10-25 15:51:00 +0700

    Add base entity. Closes #10.

--- pet-clinic-data/src/main/java/com/bca/spring/mypetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/bca/spring/mypetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.bca.spring.mypetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/bca/spring/mypetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/bca/spring/mypetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.bca.spring.mypetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
