104c7b8902f63ec308738b165494b3ba7082f0c5
Kasih Handoyo <kasih_handoyo@bca.co.id>
2022-11-04 06:17:33 +0700

    Refactor to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/com/bca/spring/mypetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/bca/spring/mypetclinic/services/PetService.java
@@ -2,10 +2,7 @@ package com.bca.spring.mypetclinic.services;
 
 import com.bca.spring.mypetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet owner);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
+
 }
--- pet-clinic-data/src/main/java/com/bca/spring/mypetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/bca/spring/mypetclinic/services/VetService.java
@@ -2,10 +2,7 @@ package com.bca.spring.mypetclinic.services;
 
 import com.bca.spring.mypetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
+
 }
